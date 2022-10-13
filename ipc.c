9595a767844a5ec064d3aed7b1d852f2e786d059
johncole27 <115480236+johncole27@users.noreply.github.com>
2022-10-13 07:56:38 +0530

    Add files via upload

--- ipc.c
+++ ipc.c
@@ -1,31 +1,27 @@
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <stdio.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<stdio.h>
 
 int main()
-{      
+{
     key_t key = ftok("shmfile",65);
-   
+    
     int shmid = shmget(key,1024,0666|IPC_CREAT);
- 	
- 	
- 	printf("Creating a new memory segment.");
- 	printf("\nShmid: %d\n",shmid);
- 	
-    char *str = (char*) shmat(shmid,(void*)0,0);
-   
-    printf("Write Data : ");
-    scanf("%[^\n]%*c", str);
- 
-    printf("Data written in memory: %s\n",str);
-     
+    
+    char *str=(char*)shmat(shmid,(void*)0,0);
+    
+    printf("Write data:");
+    scanf("%[^\n]%*c",str);
+    
     shmdt(str);
-     
-    char *s = (char*) shmat(shmid,(void*)0,0);
- 
-    printf("Data read from memory: %s\n",s);
+    
+    char *s=(char*)shmat(shmid,(void*)0,0);
+    
+    printf("data read from memory:%s\n",s);
+    
     shmdt(s);
-   	shmctl(shmid,IPC_RMID,NULL);
- 
-    return 0;
+    
+    shmctl(shmid,IPC_RMID,NULL);
+    
+    return 0; 
 }
